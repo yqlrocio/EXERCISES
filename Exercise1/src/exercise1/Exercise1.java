ef656bd73e991aeacf832571e70d2cf92fae6dd6
rocio <rocio@Roco>
2025-09-19 19:39:56 +0200

    Estamos comiteando el primer ejercicio en inglés de programación

---
+++ Exercise1/src/exercise1/Exercise1.java
@@ -0,0 +1,11 @@
+package exercise1;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		System.out.print("Hello,  how are you? ");
+		System.out.println("Fine thanks.");
+
+	}
+
+}
