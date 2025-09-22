6a49ec0709a6aa95c80090751897545d7c3e6473
rocio <rocio@Roco>
2025-09-22 18:06:18 +0200

    primer intento

---
+++ Exercise6/src/exercise6/Exercise6.java
@@ -0,0 +1,12 @@
+package exercise6;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		System.out.print("My name is Qiaolin Rocío.");
+		System.out.println("Right now I am living in Sevilla, specifically on the street Beatriz de Suavia\n");
+		System.out.print("744658009");
+
+	}
+
+}
