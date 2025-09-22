fefeeb6a303d2237af12c8320ce6dd14fec3f108
rocio <rocio@Roco>
2025-09-22 18:04:48 +0200

    añadiendo espacio

--- Exercise5/src/exercise5/Exercise5.java
+++ Exercise5/src/exercise5/Exercise5.java
@@ -3,9 +3,9 @@ package exercise5;
 public class Exercise5 {
 
 	public static void main(String[] args) {
-		System.out.print("My name is Qiaolin Rocío");
-		System.out.println("Right now I am living in Sevilla, specifically on the street Beatriz de Suavia");
-		System.out.print("744658009");
+		System.out.print("My name is Qiaolin Rocío\n");
+		System.out.println("Right now I am living in Sevilla, specifically on the street Beatriz de Suavia\n");
+		System.out.print("744658009\n");
 
 
 	}
