349271ef3dc5bc057f0807c23ce9e2b6cd287177
qr.ye <qr.ye@106-29.info2>
2025-10-01 10:07:55 +0200

    falta por terminar 2 y 7

---
+++ Excercises/src/excercises/Exercise03.java
@@ -0,0 +1,9 @@
+package excercises;
+
+public class Exercise03 {
+
+	public static void main(String[] args) {
+		System.out.println("2 + 2 " + "+ 3" + " = 7");
+	}
+
+}
---
+++ Excercises/src/excercises/Exercise04.java
@@ -0,0 +1,10 @@
+package excercises;
+
+public class Exercise04 {
+
+	public static void main(String[] args) {
+		System.out.println("I want to learn new lenguage"); 
+
+	}
+
+}
