a9c42b050956d4849d52467f233f0aa91c904ce3
qr.ye <qr.ye@106-29.info2>
2025-10-01 09:22:49 +0200

    Creando nuevo repositorio y proyecto para guardar todos los ejercicios en ingles

---
+++ Excercises/src/excercises/Exercise01.java
@@ -0,0 +1,23 @@
+package excercises;
+
+import java.util.Scanner;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		// Create variable
+		int age;
+		
+		// Create Scanner
+		Scanner reader = new Scanner(System.in); 
+		
+		// Ask to the user about him/her age
+		System.out.print("Hello,  how are you? ");
+		age = reader.nextInt();
+		
+		System.out.println("Fine thanks.");
+
+
+	}
+
+}
