daf7868a0725f33ef8a26eaccafcae32b92637c6
rocio <rocio@Roco>
2025-10-02 22:29:02 +0200

    Finalizado

--- Excercises/src/exercisesPart2/Exercise01.java
+++ Excercises/src/exercisesPart2/Exercise01.java
@@ -1,5 +1,7 @@
 package exercisesPart2;
 
+import java.util.Scanner;
+
 public class Exercise01 {
 
 	public static void main(String[] args) {
@@ -13,8 +15,32 @@ public class Exercise01 {
 		 * Mostrar precio nuevo
 		 * Fin
 		 */
-		
+		// Create new Scanner
+				Scanner sc = new Scanner(System.in);
+				
+				//Create variables 
+				float price; 
+				int tax; 
+				
+				//Display program title
+				System.out.println("Excersice1");
+				
+				//Display prompt for price 
+				System.out.print("Enter the price: ");
+				price = sc.nextFloat();
+				
+				//Display prompt for tax 
+				System.out.print("Enter the tax: ");
+				tax = sc.nextInt();
+				
+				//Set price to price * tax 
+				price = price * (1+(tax/100.0f));		
+				
+				//Print result 
+				System.out.println("The prices with taxes is: " + price );
+				
+				//Close Scanner
+				sc.close();
+			}
 
-	}
-
-}
+		}
